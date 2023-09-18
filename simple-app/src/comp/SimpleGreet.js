c7dc35dcf5684c79abcb21d9483300e2650fb657
tojo1337 <tojo.mukherjee007@gmail.com>
2023-09-18 18:50:23 +0530

    6th commit

--- simple-app/src/comp/SimpleGreet.js
+++ simple-app/src/comp/SimpleGreet.js
@@ -7,7 +7,9 @@ const SimpleGreet = ()=>{
 	const setDataUrl = "http://localhost:8080/setter";
 	const [todoList,setTodoList] = useState([]);
 	const [show,setShow] = useState(false);
-	const [editVal,setEditVal] = useState(new Map());
+	const [editVal,setEditVal] = useState({"editId":-1,"editData":""});
+	const [visible,setVisible] = useState(false);
+	const [delParam,setDelParam] = useState({"delId":-1,"delData":""});
 	const dataFetch = (data,setFunc)=>{
 		let arr = data.data;
 		let setData = [];
@@ -35,19 +37,28 @@ const SimpleGreet = ()=>{
 	}
 	const editHandle = (event,keyVal)=>{
 		let arr = [...todoList];
-		let map = new Map();
-		map.set(arr[keyVal],keyVal);
-		setEditVal(map);
+		let targetData = arr[keyVal];
+		setEditVal({"editId":keyVal,"editData":targetData});
 		setShow(true);
 	}
-	const ModalShow = ()=>{
+	const EditShow = ()=>{
 		let handleClose = ()=>{setShow(false)}
 		let editSubmitHandler = (e)=>{
 			e.preventDefault();
-			let arr = [...todoList];
-			let mappedData = [...editVal];
-			console.log(mappedData.entries().next());
-			setEditVal(new Map());
+			var arr = [...todoList];
+			var form = e.target;
+			var formData = new FormData(form);
+			var formJson = Object.fromEntries(formData.entries());
+			var id = editVal.editId;
+			var data = editVal.editData;
+			for(var i=0;i<arr.length;i++){
+				if((i===id)&&(data===arr[i])){
+					arr[i] = formJson.myData;
+				}
+			}
+			setTodoList(arr);
+			setEditVal({"editId":-1,"editData":""});
+			setShow(false);
 		}
 		return(
 			<Modal show={show} onHide={handleClose}>
@@ -57,7 +68,7 @@ const SimpleGreet = ()=>{
 				<Modal.Body>
 					<form className="form-grid" method="post" onSubmit={editSubmitHandler}>
 						<div className="row">
-							<input className="form-control" type="text" placeholder={editVal} disabled />
+							<input className="form-control" type="text" placeholder={editVal.editData} disabled />
 						</div>
 						<div className="row">
 							<input className="form-control" type="text" name="myData" />
@@ -74,17 +85,42 @@ const SimpleGreet = ()=>{
 		)
 	}
 	const removeHandle = (event,keyVal)=>{
-		//Add the delete handler in here
 		let arr = [...todoList];
-		let newArr = [];
-		for(let i=0;i<arr.length;i++){
-			if(i===keyVal){
-				continue;
-			}else {
-				newArr.push(arr[i]);
-			}
+		setDelParam({"delId":keyVal,"delData":arr[keyVal]});
+		setVisible(true);
+	}
+	const DelShow = ()=>{
+		let cancelHandle = ()=>{
+			setVisible(false);
 		}
-		setTodoList(newArr);
+		let delHandle = ()=>{
+			let arr = [...todoList];
+			let newArr = [];
+			for(var i=0;i<arr.length;i++){
+				if(i===delParam.delId){
+					continue;
+				}else {
+					newArr.push(arr[i]);
+				}
+			}
+			setTodoList(newArr);
+			setDelParam({"delId":-1,"delData":""});
+			setVisible(false);
+		}
+		return(
+			<Modal show={visible} onHide={cancelHandle}>
+				<Modal.Header>
+					Alert!!!
+				</Modal.Header>
+				<Modal.Body>
+					Are you willing to continue ?
+				</Modal.Body>
+				<Modal.Footer>
+					<button className="btn btn-outline-warning" onClick={delHandle}>continue</button>
+					<button className="btn btn-outline-primary" onClick={cancelHandle}>cancel</button>
+				</Modal.Footer>
+			</Modal>
+		)
 	}
 	const List = ()=>{
 		let arr = [...todoList];
@@ -111,6 +147,8 @@ const SimpleGreet = ()=>{
 	const saveHandle = (evt)=>{
 		evt.preventDefault();
 		console.log("This will send data to : "+setDataUrl);
+		// Parse the data in json format and send it to the url
+		// Need to know how to handle array object being returned as json in java/spring boot
 	}
 	return(
 		<div className="container simple-greet">
@@ -126,7 +164,8 @@ const SimpleGreet = ()=>{
 				</div>
 			</form>
 			<List />
-			<ModalShow />
+			<EditShow />
+			<DelShow />
 		</div>
 	)
 }
